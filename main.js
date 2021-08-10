16aab729ecd8cb7d0efc7e3891635d009b649fc9
Jeremiah <jsnidar@gmail.com>
2021-08-10 15:16:45 -0500

    commit tree 6c4e581edf474db71617cc4c3b32d6c68eafddd3

--- main.js
+++ main.js
@@ -3,9 +3,55 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+// When a user clicks on an empty heart:
+  // Invoke mimicServerCall to simulate making a server request    
+  const likerButtons = document.querySelectorAll('.like-glyph')
+  for (let inst of likerButtons) {
+    inst.addEventListener('click', handleClickOnLike)
+  }
 
+  function handleClickOnLike (e) {
+    let heart = e.target
+    mimicServerCall()
+    .then(() => {
+      if(heart.textContent === EMPTY_HEART)
+        //if its empty
+        // When the "server" returns a success status:
+        // Change the heart to a full heart
+        // Add the .activated-heart class to make the heart appear red
+      {
+        heart.textContent = FULL_HEART
+        heart.classList.add('activated-heart')
+      }else if (heart.textContent === FULL_HEART) {
+        //if its full
+        // When a user clicks on a full heart:
+          // Change the heart back to an empty heart
+          // Remove the .activated-heart class
+        heart.classList.remove('activated-heart')
+        heart.textContent = EMPTY_HEART
+      }
+    })
+// When the "server" returns a failure status:
+  // Respond to the error using a .catch(() => {}) block after your .then(() => {}) block.
+  
+  
+    .catch( (e) => {
+      debugger
+      // Display the error modal by removing the .hidden class
+      const modal = document.querySelector('#modal')
+      modal.classList.remove('hidden')
+      // Display the server error message in the modal
+      const message = document.querySelector('#modal-message')
+      message.innerText = e
 
-
+      // Use setTimeout to hide the modal after 3 seconds (add the .hidden class)
+      setTimeout(document.getElementById('modal').className = 'hidden', 3000)
+    })
+  }
+  
+  
+  // Keep all your styling rules entirely in style.css. Do not manipulate any .style properties.
+  // Only manipulate the DOM once the server request responds. Do not make the heart full until you're inside a successful .then block.
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
